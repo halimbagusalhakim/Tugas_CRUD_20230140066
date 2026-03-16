8ff4800ec3047c191b32672a8a8867d47413db26
halimbagusalhakim <halsat132@gmail.com>
2026-03-16 09:52:45 +0700

    feat: add KtpRepository for database operations
    - Create KtpRepository interface
    - Extend JpaRepository<Ktp, Integer>
    - Add existsByNomorKtp method to check duplicate KTP number

---
+++ src/main/java/com/example/ktp/repository/KtpRepository.java
@@ -0,0 +1,10 @@
+package com.example.ktp.repository;
+
+import com.example.ktp.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    boolean existsByNomorKtp(String nomorKtp);
+}
